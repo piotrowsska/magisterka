28d1e40d87c82c37d3024254b4c2fd9820cf124d
krzysztof-github <krzysztof.klusek.1997@tlen.pl>
2024-06-11 20:09:28 +0200

    auth

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,26 +3,32 @@
     <div
       class="w-[500px] py-10 px-16 rounded-md bg-white bg-opacity-40 shadow-2xl"
     >
-      <div v-if="!resetPassword" class="flex flex-col items-center">
+      <div v-if="!state.resetPassword" class="flex flex-col items-center">
         <p class="flex justify-center pb-24 text-violet text-5xl uppercase">
           Logowanie
         </p>
         <input
           type="email"
+          v-model="state.email"
           placeholder="Podaj email..."
           class="w-full h-16 px-4 bg-white rounded-2xl text-black text-xl focus:outline-none mb-10"
         />
         <input
           type="password"
+          v-model="state.password"
           placeholder="Podaj hasło..."
           class="w-full h-16 px-4 bg-white rounded-2xl text-black text-xl focus:outline-none mb-14"
         />
         <button
+          @click="singIn"
           class="h-16 justify-center bg-violet text-2xl px-10 rounded-2xl mb-5"
         >
           Zaloguj
         </button>
-        <p @click="resetPassword = true" class="text-violet cursor-pointer">
+        <p
+          @click="state.resetPassword = true"
+          class="text-violet cursor-pointer"
+        >
           Nie pamiętasz hasła?
         </p>
       </div>
@@ -46,7 +52,25 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { reactive } from "vue";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "vue-router";
 
-const resetPassword = ref(false);
+const router = useRouter();
+
+const state = reactive({
+  resetPassword: false,
+  email: "",
+  password: "",
+  errorMessage: "",
+});
+
+const singIn = async () => {
+  try {
+    await signInWithEmailAndPassword(getAuth(), state.email, state.password);
+    router.push("/panel");
+  } catch (error: any) {
+    alert(error.code);
+  }
+};
 </script>
---
+++ src/views/PanelView.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>{{ auth.currentUser?.email }}</p>
+</template>
+
+<script setup lang="ts">
+import { getAuth } from "firebase/auth";
+
+const auth = getAuth();
+</script>
